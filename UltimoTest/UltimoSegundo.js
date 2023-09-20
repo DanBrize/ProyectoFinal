d0dd6bb1f3fd8e00657e14e8227e41d771c50ed8
DanBrize <138539364+DanBrize@users.noreply.github.com>
2023-09-20 17:35:59 -0300

    Add files via upload

---
+++ UltimoTest/UltimoSegundo.js
@@ -0,0 +1,24 @@
+// let user = document.querySelector("#user");
+// let password = document.querySelector("#password");
+// let logInForm = document.querySelector("#ingreso");
+
+// function validarformulario() {
+//     var usuario = document.querySelector("#usuario").value; // Debes obtener el valor del campo de usuario
+//     var contrasenia = document.querySelector("#contrasenia").value; // Debes obtener el valor del campo de contraseña
+
+//     if (usuario === "admin" && contrasenia === "admin123") { // Debes usar && en lugar de || para comprobar ambas condiciones
+//         document.getElementById("mensaje").innerHTML = "Por favor ingrese los datos nuevamente";
+//         console.log("test");
+//         return false;
+//     } else {
+//         document.getElementById("mensaje").innerHTML = "";
+//         console.log("abajo"); // Limpia el mensaje de error si las credenciales son válidas
+//         return true;
+//     }
+// }
+
+// document.addEventListener("DOMContentLoaded"), function () {
+
+
+    
+// }
